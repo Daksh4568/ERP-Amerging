e0d19936ae273318321887ee0b038f05f9862d86
Daksh Pratap Singh <daksh6666singh@gmail.com>
2025-01-09 16:13:36 +0530

    Updated the notification schema and notification format

--- Backend/src/Employee Module/models/notificationSchema.js
+++ Backend/src/Employee Module/models/notificationSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
-
+// I am also sending the leave id with the data t ensure that the particular leave is approved 
 const notificationSchema = new mongoose.Schema({
     notificationId: { type: String, unique: true, default: uuidv4 },
     recipientEmpId: { type: String, required: true },
@@ -10,8 +10,12 @@ const notificationSchema = new mongoose.Schema({
     message: { type: String, required: true },
     status: { type: String, enum: ['Approved', 'Rejected', 'Pending'], select: false },
     data: { leaveId: { type: mongoose.Schema.Types.ObjectId, ref: 'LeaveApplication' }, additionalInfo: { type: Object } },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    reasonForLeave: { type: String },
     isRead: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
+
 });
 
 const Notification = mongoose.model('Notification', notificationSchema);
