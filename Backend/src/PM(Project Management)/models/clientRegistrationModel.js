f704c2c964f2c399e7c35da8b12d3ceb162d39fe
Daksh Pratap Singh <daksh6666singh@gmail.com>
2025-04-05 12:30:00 +0530

    Updated the model for the client registration form

--- Backend/src/PM(Project Management)/models/clientRegistrationModel.js
+++ Backend/src/PM(Project Management)/models/clientRegistrationModel.js
@@ -4,10 +4,11 @@ const LeadSchema = new mongoose.Schema(
     {
         leadOwner: {
             type: String,
-            // Assuming lead owner is mandatory
+            required: true// Assuming lead owner is mandatory
         },
         firstName: {
             type: String,
+            required: true
         },
         lastName: {
             type: String,
@@ -21,37 +22,62 @@ const LeadSchema = new mongoose.Schema(
                 },
                 message: "Invalid email format!",
             },
+            required: true
         },
         phone: {
             type: String,
         },
         mobile: {
             type: String,
+            required: true
         },
         company: {
             type: String,
-            required: true, // Marked red, indicating mandatory
+            required: true,
         },
         leadStatus: {
             type: String,
             enum: ["Attempted to Contact", "Contacted", "Quotation pending", "Quotation created", "Contact in Future", "Quotation Send", "Junk Lead", "Not Contacted", "Pre Qualified", "Not Qualified"], // Modify as per system logic
             default: "New",
+            required: true
         },
         leadSource: {
             type: String,
             enum: ["Advertisement", "Referral", "Website", "Cold Call", "Other"], // Modify based on lead sources
+            required: true
         },
         leadThrough: {
             type: String,
+            required: true
         },
         quotationNumber: {
             type: String,
+            required: true
         },
         contactCreated: {
             type: Date,
         },
         quotationSendDate: {
             type: Date,
+        },
+        description: {
+            type: String
+        },
+        address: {
+            street: String,
+            city: String,
+            state: String,
+            postalCode: {
+                type: String,
+                required: true,
+                validate: {
+                    validator: function (v) {
+                        return /\d{6}/.test(v); // Validate that postal code is 6 digits
+                    },
+                    message: (props) => `${props.value} is not a valid postal code!`,
+                },
+            },
+            country: String,
         },
     },
     { timestamps: true } // Auto-adds createdAt and updatedAt
