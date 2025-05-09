59ba522a6d28cf46a6337875f61e83550ad0988b
Daksh Pratap Singh <73421193+Daksh4568@users.noreply.github.com>
2025-05-09 09:33:20 +0000

    Updated the addedby for tour expense

--- Backend/src/Employee Module/models/empTourExpense.js
+++ Backend/src/Employee Module/models/empTourExpense.js
@@ -43,7 +43,7 @@ const tourExpenseItemSchema = new mongoose.Schema({
     hrRemark: {
         type: String
     },
-    addedBy: { name: { type: String, required: true }, role: { type: String, required: true } },
+    addedBy: { name: { type: String, required: true }, role: { type: String } },
     createdAt: {
         type: Date,
         default: Date.now
